95d36802cefec45ddb909ab71fb43c26a2b3066a
Minseok Bae <minseok@vectara.com>
2024-01-03 00:24:51 +0900

    Test 1 for Elastic Beanstalk integration...

--- application.py
+++ application.py
@@ -1,6 +1,19 @@
-from app import create_app
+from flask import Flask, render_template, request
+import app.utils as utils
 
-application = create_app()
+application = Flask(__name__)
 
-if __name__ == '__main__':
+@application.route('/')
+def index():
+    return render_template('index.html')
+
+@application.route('/submit', methods=['GET'])
+def submit():
+    return render_template('submission.html')
+
+@application.route('/upload', methods=['POST'])
+def upload_file():
+    return utils.upload_file_logic(request)
+
+if __name__ == "__main__":
     application.run(debug=True)
